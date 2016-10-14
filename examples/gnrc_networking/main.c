7ff556b9ce58f6614da4935e514a63e937700760
smlng <s@mlng.net>
2016-10-14 11:40:10 +0200

    revert changes

--- examples/gnrc_networking/main.c
+++ examples/gnrc_networking/main.c
@@ -20,16 +20,10 @@
 
 #include <stdio.h>
 
-#include "net/gnrc/netapi.h"
-#include "net/gnrc/netif.h"
 #include "shell.h"
 #include "msg.h"
 
 #define MAIN_QUEUE_SIZE     (8)
-#ifndef DEFAULT_TX_POWER
-#define DEFAULT_TX_POWER    (0)
-#endif
-
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 extern int udp_cmd(int argc, char **argv);
@@ -39,25 +33,13 @@ static const shell_command_t shell_commands[] = {
     { NULL, NULL, NULL }
 };
 
-static void _set_tx_power(void)
-{
-    kernel_pid_t ifs[GNRC_NETIF_NUMOF];
-    int16_t tx_power = DEFAULT_TX_POWER;
-    /* get the PID of the first radio */
-    if (gnrc_netif_get(ifs) <= 0) {
-        puts("ERROR: comm init, not radio found!\n");
-        return;
-    }
-    gnrc_netapi_set(ifs[0], NETOPT_TX_POWER, 0, (int16_t *)&tx_power, 2);
-}
-
 int main(void)
 {
     /* we need a message queue for the thread running the shell in order to
      * receive potentially fast incoming networking packets */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     puts("RIOT network stack example application");
-    _set_tx_power();
+
     /* start shell */
     puts("All up, running the shell now");
     char line_buf[SHELL_DEFAULT_BUFSIZE];
